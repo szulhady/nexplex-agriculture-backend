d16006ca64845213c7917975ee34963aea7eb8c3
szulhady <63948136+szulhady@users.noreply.github.com>
2021-11-02 10:24:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -618,6 +618,46 @@ app.get('/api/schedule/kongPo',(req,res)=>{
   });
 })
 
+app.get('/api/schedule/manong',(req,res)=>{
+  dat = [];
+  // var q = `SELECT * FROM manong_schedule  order by date asc`;
+  var q = `SELECT * FROM manong_schedule WHERE date >= CURDATE() order by date asc`;
+  // connection.connect();
+  connection.query(q, function (error, row, fields) {
+    if (error) {
+      console.log(error);
+    }
+    if (row) {
+      for (var i = 0; i < row.length; i++) {
+        let data2=[]
+        let timeArray  = row[i].time.split(',')
+        let durationArray  = row[i].duration.split(',')
+        let substanceArray = row[i].substance.split(',')
+        // console.log(row[i])
+        timeArray.forEach((element,index) => {
+          data2.push(` ${index+1}) [ Time : ${element}, Duration : ${durationArray[index]} min, Substance : ${substanceArray[index]}  ], `)
+        });
+        // console.log(data2)
+        let data = {
+          date:row[i].date,
+          // time:row[i].time,
+          // block:row[i].block,
+          // duration:row[i].duration,
+          remarks:data2
+        }
+        dat.push(
+          data
+        );
+        // console.log(dat)
+      }
+      ipah1=dat
+    }
+    ret = JSON.stringify(ipah1)
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send(ret)
+  });
+})
+
 // NUTREINT PREPARATION //
 app.get('/api/schedule/ipah1/nutrient',(req,res)=>{
   dat = [];
@@ -738,6 +778,35 @@ app.get('/api/schedule/kongPo/nutrient',(req,res)=>{
   });
 })
 
+app.get('/api/schedule/manong/nutrient',(req,res)=>{
+  dat = [];
+  var q = `SELECT * FROM manong_nutrient_schedule WHERE date >= CURDATE() order by date asc`;
+  // connection.connect();
+  connection.query(q, function (error, row, fields) {
+    if (error) {
+      console.log(error);
+    }
+    if (row) {
+      for (var i = 0; i < row.length; i++) {
+        let data2=[]
+        data2.push(`Duration : ${row[i].duration} minutes`)
+        let data = {
+          date:row[i].date,
+          time:row[i].time,
+          remarks:data2,
+        }
+        dat.push(
+          data
+        );
+      }
+      ipah1=dat
+    }
+    ret = JSON.stringify(ipah1)
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send(ret)
+  });
+})
+
 // POST //
 // DRIPPING WATER/NUTRIENT //
 app.post('/api/setSchedule/ipah1',(req,res)=>{
@@ -812,6 +881,42 @@ app.post('/api/setSchedule/kongPo',(req,res)=>{
         client.publish('debug/test/database/ipah1', 'updated')
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.send('Sucess')
+      });
+  ;
+})
+
+app.post('/api/setSchedule/manong',(req,res)=>{
+  let dateArray = req.body.params.date
+  let time = req.body.params.time
+  let duration = req.body.params.duration
+  let substance = req.body.params.substance
+  let status=''
+
+  let stringMysql = ''
+  dateArray.forEach((date, index, array)=>{
+    // console.log('here')
+    if(index === array.length - 1){
+      stringMysql =stringMysql + `('${date}','${time}', '${duration}', '${substance}')`
+    }else{
+      stringMysql =stringMysql + `('${date}','${time}', '${duration}', '${substance}'),`
+    }
+  })
+  // console.log(stringMysql)
+      // ret = JSON.stringify(ipah1)
+      // var w = `INSERT INTO kongpo_schedule (date,time,duration, substance) VALUES ('${req.body.date}', '${req.body.time}', '${req.body.duration}', '${req.body.substance}')`;
+      var q = `INSERT INTO manong_schedule (date,time,duration, substance) VALUES ${stringMysql}`;
+      connection.query(q, function (error, row, fields) {
+        if (error) {
+          console.log(error);
+          status = error.sqlMessage
+        }
+        if (row) {
+          status = 'Success'
+        //  console.log(row)
+        }
+      //   client.publish('debug/test/database/ipah1', 'updated')
+      //   res.header('Content-Type', 'application/json; charset=utf-8')
+        res.send(status)
       });
   ;
 })
@@ -894,6 +999,41 @@ app.post('/api/setSchedule/kongPo/nutrient',(req,res)=>{
   ;
 })
 
+app.post('/api/setSchedule/manong/nutrient',(req,res)=>{
+  // console.log(req.body.date)
+      // ret = JSON.stringify(ipah1)
+      let dateArray = req.body.params.date
+      let duration = req.body.params.duration
+
+      let stringMysql = ''
+      let status=''
+      dateArray.forEach((date, index, array)=>{
+        // console.log('here')
+        if(index === array.length - 1){
+
+          stringMysql =stringMysql + `('${date}','05:00:00', '${duration}')`
+        }else{
+          stringMysql =stringMysql + `('${date}','05:00:00', '${duration}'),`
+        }
+      })
+
+      var q = `INSERT INTO manong_nutrient_schedule (date,time, duration) VALUES  ${stringMysql}`;
+      connection.query(q, function (error, row, fields) {
+        if (error) {
+          console.log(error);
+          status = error.sqlMessage
+        }
+        if (row) {
+          console.log('post manong nutrient success')
+          status = 'Success'
+        //  console.log(row)
+        }
+        // client.publish('debug/test/database/kongPo', 'updated')
+        res.header('Content-Type', 'application/json; charset=utf-8')
+        res.send(status)
+      });
+  ;
+}) 
 
 // DELETE //
 // DRIPPING WATER/NUTRIENT //
@@ -957,6 +1097,21 @@ app.delete('/api/schedule/kongPo',(req,res)=>{
       });
 })
 
+app.delete('/api/schedule/manong',(req,res)=>{
+  dat = [];
+  var q = `DELETE FROM manong_schedule WHERE date = "${req.body.date}"`;
+  // connection.connect();
+  connection.query(q, function (error, row, fields) {
+    if (error) {
+      console.log(error);
+    }
+    if (row) {
+      res.json('deleted')
+
+    }
+  });
+})
+
 //  NUTRIENT PREPARATION //
 app.delete('/api/schedule/ipah1/nutrient',(req,res)=>{
       dat = [];
@@ -1015,6 +1170,19 @@ app.delete('/api/schedule/kongPo/nutrient',(req,res)=>{
   });
 })
 
+app.delete('/api/schedule/manong/nutrient',(req,res)=>{
+  dat = [];
+  var q = `DELETE FROM manong_nutrient_schedule WHERE date = "${req.body.date}"`;
+  // connection.connect();
+  connection.query(q, function (error, row, fields) {
+    if (error) {
+      console.log(error);
+    }
+    if (row) {
+      res.json('deleted')
+    }
+  });
+})
 
 // REPORT GENERATOR //
 app.post('/api/report/form',(req,res)=>{
@@ -1227,3 +1395,7 @@ getUpdatedDataTkpmIpahNutrient()
 getUpdatedDataTkpmPagohNutrient()
 getUpdatedDataKongPoNutrient()
 },6000)
+
+app.post("/try/react",(req,res)=>{
+  console.log(req.body.params)
+})
